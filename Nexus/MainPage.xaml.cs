ea444ed112f42e02caee6eb5ddecbb1cd3b0d6ca
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2024-11-07 11:00:19 +0100

    First Commit (Projet Solution)

---
+++ Nexus/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Nexus;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Nexus/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Nexus;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
